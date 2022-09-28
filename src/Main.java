f0b7372151da7db35f22f06e4196efbea7d3593a
saint <ruslan.meloman.1986@yandex.ru>
2022-09-28 16:47:08 +0200

    Выполнение ДЗ Задания 4, 5

--- src/Main.java
+++ src/Main.java
@@ -27,5 +27,25 @@ public class Main {
         System.out.println(cat);
         paper = paper - 7639;
         System.out.println(paper);
+
+        // Задача 4
+        System.out.println("Задача 4");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        // Задача 5
+        System.out.println("Задача 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
     }
 }
