ac1cf562b97787fe98b8abddfd72ac5bdb7a0484
saint <ruslan.meloman.1986@yandex.ru>
2022-09-28 16:43:42 +0200

    Выполнение ДЗ Задания 1-3

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задача 1
+        System.out.println("Задача 1");
+        var dog = 8;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+
+        // Задача 2
+        System.out.println("Задача 2");
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+
+        // Задача 3
+        System.out.println("Задача 3");
+        var dog1 = 8.0;
+        dog1 = dog1 - 3.5;
+        System.out.println(dog1);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+    }
+}
